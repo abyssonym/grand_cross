7ba7fa2a17e4c6a3f7e1277571d548acf62a0a04
Anonymous <none>
2016-08-29 00:44:39 -0700

    add basic job objects

---
+++ randomizer.py
@@ -0,0 +1,65 @@
+from randomtools.tablereader import TableObject, get_global_label, tblpath
+from randomtools.utils import (
+    classproperty, mutate_normal, shuffle_bits,
+    utilrandom as random)
+from randomtools.interface import (
+    get_outfile, get_seed, get_flags, run_interface, rewrite_snes_meta,
+    clean_and_write, finish_interface)
+from collections import defaultdict
+from os import path
+
+
+VERSION = 1
+ALL_OBJECTS = None
+
+
+class JobAbilityObject(TableObject):
+    @classproperty
+    def every(cls):
+        if hasattr(cls, "_every"):
+            return cls._every
+        cls._every = super(JobAbilityObject, cls).every
+        mimic = JobAbilityObject(get_outfile(), 0x115429, index=99,
+                                 groupindex=20)
+        cls._every.append(mimic)
+        return cls.every
+
+class AbilityCountObject(TableObject): pass
+class JobStatsObject(TableObject):
+    mutate_attributes = {
+        "strength": None,
+        "agility": None,
+        "stamina": None,
+        "magic": None,
+        }
+
+class JobEquipObject(TableObject): pass
+class JobInnatesObject(TableObject): pass
+
+
+if __name__ == "__main__":
+    try:
+        print ('You are using the FF5 '
+               'randomizer version %s.' % VERSION)
+        ALL_OBJECTS = [g for g in globals().values()
+                       if isinstance(g, type) and issubclass(g, TableObject)
+                       and g not in [TableObject]]
+        run_interface(ALL_OBJECTS, snes=True)
+        '''
+        mimic = JobAbilityObject.groups[20]
+        knight = JobAbilityObject.groups[0]
+        for ja in mimic:
+            ja.groupindex = 0
+        for ja in knight:
+            ja.groupindex = 20
+        '''
+        hexify = lambda x: "{0:0>2}".format("%x" % x)
+        numify = lambda x: "{0: >3}".format(x)
+        minmax = lambda x: (min(x), max(x))
+        clean_and_write(ALL_OBJECTS)
+        rewrite_snes_meta("FF5-R", VERSION, megabits=32, lorom=False)
+        finish_interface()
+        import pdb; pdb.set_trace()
+    except AssertionError, e:
+        print "ERROR: %s" % e
+        raw_input("Press Enter to close this program.")
